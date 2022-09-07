0609e49a08ffda3fa4d8725114b7215a492c18fe
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-07 20:18:02 +0600

    products added

--- src/script.js
+++ src/script.js
@@ -5,6 +5,135 @@ const items = [
     price: 29.9,
     img: "https://m.media-amazon.com/images/I/816JswG0XbL._AC_UY550_.jpg",
   },
+  {
+    title:
+      "Anne Cole Women's Solid Twist Front Shirred Bandeau Tankini Swim Top",
+    price: 18.0,
+    img: "https://m.media-amazon.com/images/I/71iFOb-FeSL._AC_UY550_.jpg",
+  },
+  {
+    title:
+      "Tommy Hilfiger Women's Bandeau Tube Top with Classic Tommy Jeans Color Block and Logo",
+    price: 10.93,
+    img: "https://m.media-amazon.com/images/I/81xHlTBI1rL._AC_UY550_.jpg",
+  },
+  {
+    title: "The Drop Women's Christy Cowl Neck Cami Silky Stretch Top",
+    price: 39.9,
+    img: "https://m.media-amazon.com/images/I/81aw-E483-L._AC_UY550_.jpg",
+  },
+  {
+    title: "Jessica Simpson Women's Melinda Scoop Neck Textured High Low Top",
+    price: 9.0,
+    img: "https://m.media-amazon.com/images/I/71efICFMXeL._AC_UY550_.jpg",
+  },
+  {
+    title: "Seafolly Women's Banded Longline Triangle Bikini Top Swimsuit",
+    price: 51.41,
+    img: "https://m.media-amazon.com/images/I/71amt8sOe2L._AC_UX466_.jpg",
+  },
+  {
+    title:
+      "Rip Curl Women's Standard Classic Surf Cross Back Bikini, Adjustable Swim Suit Top",
+    price: 25.3,
+    img: "https://m.media-amazon.com/images/I/71KrW0xqq2L._AC_UY550_.jpg",
+  },
+  {
+    title: "Levi's Women's Mid Length Shorts",
+    price: 25,
+    img: "https://m.media-amazon.com/images/I/51lrRUxVSNL._AC_UX522_.jpg",
+  },
+  {
+    title: "Hanes Sport Men's Performance Muscle Tee",
+    price: 8.39,
+    img: "https://m.media-amazon.com/images/I/81BHZa2boZL._AC_UY550_.jpg",
+  },
+  {
+    title:
+      "Callaway Men's Pro Spin Fine Line Short Sleeve Golf Shirt (Size X-Small-4X Big & Tall)",
+    price: 39.0,
+    img: "https://m.media-amazon.com/images/I/61ZuFapuBlL._AC_UX522_.jpg",
+  },
+  {
+    title: "Columbia Men's Leadville Ridge Short Sleeve Shirt Ii",
+    price: 19.98,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71cloLsg-8L.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+  },
+  {
+    title: "Hugo Boss Men's Regular Fit Reversed Logo Short Sleeve Polo Shirt",
+    price: 32.4,
+    img: "https://m.media-amazon.com/images/I/81Nwu322xrL._AC_UY550_.jpg",
+  },
+  {
+    title:
+      "The Drop Women's Gabriela High Neck Cut-In A-Line Side Slit Maxi Sweater Dress",
+    price: 49.9,
+    img: "https://m.media-amazon.com/images/I/71ehqv1vzKL._AC_UY550_.jpg",
+  },
+  {
+    title:
+      "Warner's Women's Cloud 9 Super Soft Wireless Lightly Lined Comfort Bra Ro5691a",
+    price: 16.0,
+    img: "https://m.media-amazon.com/images/I/81YzTOVUjBL._AC_UX569_.jpg",
+  },
+  {
+    title:
+      "The Drop Women's Mariko Puff-Sleeve Crew-Neck Stretch Jersey T-Shirt",
+    price: 16.3,
+    img: "https://m.media-amazon.com/images/I/91w3TMrcOSL._AC_UY550_.jpg",
+  },
+  {
+    title:
+      "Floerns Women's Leopard Print Color Block T Shirts Short Sleeve Round Neck Tee Tops",
+    price: 14.99,
+    img: "https://m.media-amazon.com/images/I/81oHRhtTRvL._AC_UY550_.jpg",
+  },
+  {
+    title:
+      "ARRIVE GUIDE Crop Top Athletic Shirts for Women Cute Sleeveless Yoga Tops Running Gym Workout Shirts",
+    price: 19.99,
+    img: "https://m.media-amazon.com/images/I/61ZUo08E-ML._AC_SX522._SX._UX._SY._UY_.jpg",
+  },
+  {
+    title:
+      "SheIn Women's Casual Self Tie Waist Frill Double Pocket Short Skirt",
+    price: 30.99,
+    img: "https://m.media-amazon.com/images/I/71-q+J42ZMS._AC_SY550._SX._UX._SY._UY_.jpg",
+  },
+  {
+    title:
+      "eoeoo Women's Ruched Bodycon Skirt High Waist Elastic Tight Sexy Club Mini Skirt",
+    price: 19.99,
+    img: "https://m.media-amazon.com/images/I/51erJlSw+KL._AC_UY550_.jpg",
+  },
+  {
+    title: "Shirts for Men Active Shirt Mens Large Tall Shirts",
+    price: 16.24,
+    img: "https://m.media-amazon.com/images/I/81AULfjj4MS._AC_UX522_.jpg",
+  },
+  {
+    title: "Wacoal Women's Halo Underwire Bra",
+    price: 24.66,
+    img: "https://m.media-amazon.com/images/I/81YpsSSXlJS._AC_UX522_.jpg",
+  },
+  {
+    title:
+      "Warner's Women's Cloud 9 Super Soft Wireless Lightly Lined Comfort Bra Ro5691a",
+    price: 16.0,
+    img: "https://m.media-amazon.com/images/I/812S2+5OBWL._AC_UX425_.jpg",
+  },
+  {
+    title:
+      "LEVAO Women Lace Underwear Sexy Breathable Hipster Panties Stretch Seamless Bikini Briefs 6 Pack",
+    price: 16.86,
+    img: "https://m.media-amazon.com/images/I/711zS-zhg4L._AC_UX679_.jpg",
+  },
+  {
+    title:
+      "Donnalla Women Sexy Lingerie Set Two Piece Lace Bra and Panty Set Bralette Sleepwear",
+    price: 16.99,
+    img: "https://m.media-amazon.com/images/I/61xu2u8rVES._AC_UY550_.jpg",
+  },
 ];
 
 // Display items
@@ -28,14 +157,16 @@ function displayItems() {
             <div class="overflow-hidden h-60 flex justify-start">
               <img src="${items[i].img}" class="w-full object-contain" />
             </div>
-            <div class="p-3">
-              <h3 class="text-xl mb-3">${items[i].title}</h3>
-              <p class="text-4xl mb-3">$<span>${items[i].price}</span></p>
+            <div class="item-body p-3 h-60 flex flex-col">
+              <h3 class="text-lg mb-auto">${items[i].title}</h3>
+              <div class="item-body-price-btn flex mb-3">
+                <p class="text-4xl">$<span>${items[i].price}</span></p>
               <a
                 href="#"
                 class="text-lg text-cyan-500 hover:text-red-500 ease-in-out duration-300"
                 >Buy &rarr;</a
               >
+              </div>
             </div>
     `;
 
